dec6d55b7c5684226aa03392805c9e0eebd33ff0
nijuyonkadesu <24ksriramv@gmail.com>
2022-09-24 06:27:01 +0530

    basic map fragment

---
+++ app/src/main/java/com/njk/safetyfirst/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.njk.safetyfirst
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.maps) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val emergencyLocation = LatLng(13.0327, 80.23)
+
+        mMap.addMarker(
+            MarkerOptions()
+                .position(emergencyLocation)
+                .title("Marker in Emergency!")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(emergencyLocation))
+    }
+}
