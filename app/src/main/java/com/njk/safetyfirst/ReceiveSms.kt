8aebeb946dcfc7196b6c49a3fa6e3fc72976129c
nijuyonkadesu <24ksriramv@gmail.com>
2022-09-25 13:37:13 +0530

    extract location

--- app/src/main/java/com/njk/safetyfirst/ReceiveSms.kt
+++ app/src/main/java/com/njk/safetyfirst/ReceiveSms.kt
@@ -6,20 +6,32 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.Telephony
 import android.widget.Toast
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class ReceiveSms : BroadcastReceiver() {
+    var latitude = 0.0
+    var longitude = 0.0
+
     override fun onReceive(context: Context, intent: Intent) {
 //        Toast.makeText(context, "Yo working, yes. now cry.", Toast.LENGTH_SHORT).show()
         if (intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
             val bundle: Bundle? = intent.getExtras()
-//            val Messages = mutableListOf<String>()
-//            var messageFrom: String
+
 
             if (bundle!=null){
                 val smsMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
                 for (message in smsMessages) {
-                    Toast.makeText(context, "Message from ${message.displayOriginatingAddress} : body ${message.messageBody}", Toast.LENGTH_SHORT)
-                        .show()
+//                    Toast.makeText(context, "Message from ${message.displayOriginatingAddress} : body ${message.messageBody}", Toast.LENGTH_SHORT).show()
+                    val extractedLocation = message.messageBody
+                    val pattern = Pattern.compile("lat/long: \\(([0-9.]+),([0-9.]+)\\)$")
+                    val matcher = pattern.matcher(extractedLocation)
+
+                    if (matcher.matches()) {
+                        latitude = matcher.group(1)!!.toDouble()
+                        longitude = matcher.group(2)!!.toDouble()
+                        Toast.makeText(context, "Message from ${message.displayOriginatingAddress} : lat/long: $latitude / $longitude", Toast.LENGTH_LONG).show()
+                    }
                 }
             }
         }
